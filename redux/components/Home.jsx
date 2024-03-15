25aa2343283142c222c5ea77fd2638a820bb1727
Prakarsh Verma <prakarsh1248@gmail.com>
2024-03-15 19:35:20 +0530

    Used prop drilling

---
+++ redux/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = ({value}) => {
+  return (
+    <div>
+        <h2>{value}</h2>
+        <button>Increment</button>
+        <button>Decrement</button>
+    </div>
+  )
+}
+
+export default Home
---
+++ redux/src/App.jsx
@@ -0,0 +1,13 @@
+import Home from "../components/Home"
+
+
+function App() {
+  const value = 20;
+  return (
+    <>
+      <Home value={value}/>
+    </>
+  )
+}
+
+export default App
